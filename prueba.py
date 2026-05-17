a6f41192c7632ea0d8553485dbd2b2848c75d269
George <jrugby1975@yahoo.com.ar>
2026-05-17 12:45:15 -0300

    first commit

---
+++ prueba.py
@@ -0,0 +1,48 @@
+A = 10
+
+B = 30
+
+SUMA = A + B
+
+print("La suma de A y B es:", SUMA)
+
+# Hace un intervalo de del resultado de la suma
+INTERVALO = range(SUMA - 5, SUMA + 5)
+
+print("El intervalo alrededor de la suma es:", list(INTERVALO))
+
+# Verifica si la suma es un número par o impar
+
+if SUMA %  2 == 0:
+    print("La suma es un número par.")
+else:
+    print("La suma es un número impar.")
+
+# Calcula el cuadrado de la suma
+
+CUADRADO = SUMA ** 2
+print("El cuadrado de la suma es:", CUADRADO)
+
+#  piedra papel tijera
+
+import random
+
+opciones = ["piedra", "papel", "tijera"]
+
+jugador = input("Elige piedra, papel o tijera: ").lower()
+
+computadora = random.choice(opciones)
+
+print("La computadora eligió:", computadora)
+
+if jugador == computadora:
+    print("Empate!")
+elif (jugador == "piedra" and computadora == "tijera") or \
+     (jugador == "papel" and computadora == "piedra") or \
+     (jugador == "tijera" and computadora == "papel"):
+    
+    print("¡Ganaste!")
+
+else:
+    print("¡Perdiste!")
+    
